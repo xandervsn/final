66a86dc3f607be7155339229da92f0b4904943ed
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2023-06-01 21:55:46 -0700

    Create bst.cpp

---
+++ bst.cpp
@@ -0,0 +1,67 @@
+#include "bst.h"
+
+bst::bst(){root = NULL;}
+
+void bst::build() {
+  srand((unsigned)time(0));
+  int arrayLength = rand()%5+10;
+  int values[arrayLength];
+  for (int a = 0; a < arrayLength; a++){values[a] = -1;}
+  for (int a = 0; a < arrayLength; a++) {
+    bool duplicate = false;
+    do {
+      duplicate = false;
+      values[a] = rand()%30+1;
+      for (int b = 0; b < arrayLength; b++) {
+        if ((a != b) && (values[a] == values[b])) {duplicate = true;}
+      }
+    } while (duplicate == true);
+  }
+  for (int a = 0; a < arrayLength; a++) {
+    node* newnode = new node();
+    newnode->data = values[a];
+    node* current = root;
+    if (root == NULL) {
+      root = newnode;
+    }
+    else {
+      while(current != NULL) {
+        if (newnode->data < current->data) {
+          if (current->left == NULL) {
+            current->left = newnode;
+            break;
+          }
+          else {
+            current = current->left;
+          }
+        }
+        else {
+          if (current->right == NULL) {
+            current->right = newnode;
+            break;
+          }
+          else {
+            current = current->right;
+          }
+        }
+      }
+    }
+  }
+}
+
+void bst::display_tree(node* current, int level) {
+  if (current->right != NULL) {display_tree(current->right, level+1);}
+  for (int a = 0; a < level; a++) {cout << "\t";}
+  cout << current->data << " (";
+  if (current->left != NULL) cout << current->left->data << ",";
+  else cout << ",";
+  if (current->right != NULL) cout << current->right->data << ")" << endl;
+  else cout << ")" << endl;
+  if (current->left != NULL) {display_tree(current->left, level+1);}
+}
+
+void bst::display() {
+  cout << "---------Tree Printout----------" << endl;
+  display_tree(root, 0);
+  cout << "--------------------------------" << endl;
+}
